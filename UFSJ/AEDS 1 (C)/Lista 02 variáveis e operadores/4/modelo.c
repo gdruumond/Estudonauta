75daed03501f82866280c69d1ee09e970798f712
Gabriel Drumond <bieldsoares@gmail.com>
2021-09-28 15:07:25 -0300

    commit

--- UFSJ/AEDS 1 (C)/Lista 02 variáveis e operadores/4/modelo.c
+++ UFSJ/AEDS 1 (C)/Lista 02 variáveis e operadores/4/modelo.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 
 void main(){
-    float area, volume, raio, pi;
+    float area, volume, raio;
 
-    pi = 3.14159265359;
+    const float PI = 3.14159265359;
 
     printf("Raio da circunferência: ");
     scanf("%f", &raio);
 
-    area = 4 * pi *raio;
-    volume = pi * raio * raio;
+    area = 4 * PI * raio;
+    volume = PI * raio * raio;
 
     printf("Area da circunferencia: %f \nVolume da circunferencia: %f", area, volume);
 }
